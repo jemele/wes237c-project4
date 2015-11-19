9b953d4de939e9fa228eac18159fb97b56911808
Joshua Emele <jemele@acm.org>
2015-11-18 23:11:02 -0800

    Correctly identify gold output

--- 1_Subcomponents/fft_stages/script.tcl
+++ 1_Subcomponents/fft_stages/script.tcl
@@ -8,7 +8,14 @@ set_top fft_stages
 add_files fft_stages.cpp
 add_files fft_stages.h
 add_files -tb fft_stages_test.cpp
-add_files -tb out.gold.dat
+add_files -tb out2.gold.dat
+add_files -tb out3.gold.dat
+add_files -tb out4.gold.dat
+add_files -tb out5.gold.dat
+add_files -tb out6.gold.dat
+add_files -tb out7.gold.dat
+add_files -tb out8.gold.dat
+add_files -tb out9.gold.dat
 open_solution "solution1"
 set_part  {xc7z020clg484-1}
 create_clock -period 10
