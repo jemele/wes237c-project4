490aca898a2c6adea7a0a8268e4c0991d9a89f4f
Joshua Emele <jemele@acm.org>
2015-11-18 23:11:42 -0800

    First stab, broken blade

--- 1_Subcomponents/fft_stages/fft_stages.cpp
+++ 1_Subcomponents/fft_stages/fft_stages.cpp
@@ -1,4 +1,5 @@
 
+#include <math.h>
 #include"fft_stages.h"
 /*
  * Function: fft_stage_first_op calculates the first stage of FFT.
@@ -14,7 +15,29 @@ OUTPUT:
 //=======================BEGIN: FFT fft_stages =========================
 void fft_stages(DTYPE X_R[SIZE], DTYPE X_I[SIZE], int stage, DTYPE OUT_R[SIZE], DTYPE OUT_I[SIZE]) {
 	
-	//Write a code that computes any arbitary stages of the FFT  
+	const int DFTpts = 1<<stage;
+	const int numBF = DFTpts/2;
+	const DTYPE e = -6.283185307178/DFTpts;
+	DTYPE a = 0.0;
+
+	for(int j=0; j<numBF; ++j) {
+
+		const DTYPE c = cos(a);
+		const DTYPE s = sin(a);
+		a = a + e;
+
+		for (int i = 0; i < SIZE; i += DFTpts) {
+			const int i_lower = i + 1;
+			const DTYPE temp_R = X_R[i_lower]*c- X_I[i_lower]*s;
+			const DTYPE temp_I = X_I[i_lower]*c+ X_R[i_lower]*s;
+
+			OUT_R[i_lower] = X_R[i] - temp_R;
+			OUT_I[i_lower] = X_I[i] - temp_I;
+			OUT_R[i] = X_R[i] + temp_R;
+			OUT_I[i] = X_I[i] + temp_I;
+		}
+	}
+
 
 }
 //=======================END: FFT fft_stages =========================
