94df6fe248332c9110b49b2f2c7d5a6d000e8ebd
Joshua Emele <jemele@acm.org>
2015-11-24 23:38:54 -0800

    Let's make it clear this is a compile time constant.

--- 2_Skeleton_Restructured/fft.cpp
+++ 2_Skeleton_Restructured/fft.cpp
@@ -48,11 +48,12 @@ void fft(DTYPE X_R[SIZE], DTYPE X_I[SIZE], DTYPE OUT_R[SIZE], DTYPE OUT_I[SIZE])
 // Reverse the bits of the input, assuming a 10-bit input.
 static unsigned int reverse(register unsigned int x)
 {
+    static const unsigned final_shift = ((sizeof(x)*8) - M);
     x = (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
     x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
     x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
     x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
-    return ((x >> 16) | (x << 16)) >> ((sizeof(x)*8) - M);
+    return ((x >> 16) | (x << 16)) >> final_shift;
 }
 
 // XXX describe this function, please
