e8b1dad5cd9f34eec41463d34e01eafbc471e21d
Joshua Emele <jemele@acm.org>
2015-11-30 20:10:14 -0800

    write i,q, output for plot and other sundry

--- host/project4_host.c
+++ host/project4_host.c
@@ -7,33 +7,53 @@
 
 int main(int argc, char **argv)
 {
-    int fdr, fdw;
-	unsigned int out[sizeof(gold_symbols)/sizeof(*gold_symbols)];
+    // open all needed files, validate later
+    int fdr = open("/dev/xillybus_read_32", O_RDONLY);
+    int fdw = open("/dev/xillybus_write_32", O_WRONLY);
 
-    // open the xillybus read/write devices
-	fdr = open("/dev/xillybus_read_32", O_RDONLY);
-	fdw = open("/dev/xillybus_write_32", O_WRONLY);
-	if ((fdr == -1) || (fdw == -1)) {
+    // validate open succeeded
+    if ((fdr == -1) || (fdw == -1)) { 
         fprintf(stderr, "open %d\n", errno);
         return errno;
-	}
-
+    }
+    
     // write iq samples and read the resulting symbols
+    unsigned int out[sizeof(gold_symbols)/sizeof(*gold_symbols)];
     write(fdw, gold_iq, sizeof(gold_iq));
     read(fdr, out, sizeof(out));
 
     // close xillybus devices
-	close(fdr);
-    fdr = -1;
-	close(fdw);
-    fdw = -1;
+    close(fdr); fdr = -1;
+    close(fdw); fdw = -1;
 
-    // validate output
+    // for plot's sake, write i/q and output
+    FILE *ifile = fopen("input_i.txt", "w");
+    FILE *qfile = fopen("input_q.txt", "w");
+    FILE *ofile = fopen("output.txt" , "w");
+    if (!ifile || !qfile || !ofile) {
+        fprintf(stderr, "open %d\n", errno);
+        return errno;
+    }
     int i;
     for (i = 0; i < sizeof(out)/sizeof(*out); ++i) {
-        printf("%d %d\n", i, out[i]);
+        fprintf(ifile, "%f,", gold_i[i]);
+        fprintf(qfile, "%f,", gold_q[i]);
+        fprintf(ofile, "%d,", out[i]);
+    }
+    fclose(ifile); ifile = 0;
+    fclose(qfile); qfile = 0;
+    fclose(ofile); ofile = 0;
+
+    // validate output
+    int errors = 0;
+    for (i = 0; i < sizeof(out)/sizeof(*out); ++i) {
+        printf("%d %d %d\n", i, out[i], gold_symbols[i]);
+        if (out[i] != gold_symbols[i]) {
+            fprintf(stderr, "symbol error\n");
+            ++errors;
+        }
     }
 
-    // and all is well
-	return 0;
+    // return the number of errors
+    return errors;
 }
