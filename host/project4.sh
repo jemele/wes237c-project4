038d8c473c28fa1c0835235bbb35d63321c4d048
Joshua Emele <jemele@acm.org>
2015-11-30 22:04:06 -0800

    lose the read

--- host/project4.sh
+++ host/project4.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 # show ourselves
 cat $0
-read
 
 # cat, then compile the host program
 rm -f project4_host
-read
 cat project4_host.c
-read
 make project4_host
 ./project4_host
-read
-
 
 # plot the output
 rm -f plot.png
